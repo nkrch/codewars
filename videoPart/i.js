a26a5ded24fb2db8f2368194cd96287878c47973
nkrch <111977977+nkrch@users.noreply.github.com>
2024-07-24 23:25:24 +0300

    фдьщык

--- videoPart/i.js
+++ videoPart/i.js
@@ -1,12 +1,28 @@
 function videoPart(part, total) {
   //coding and coding..
   let partTime = part.split(":");
-  let partTimeSec = (partTime[0] * 60 + partTime[1]) * 60 + partTime[2];
+  console.log(partTime);
+  let partTimeSec =
+    (partTime[0] * 60 + partTime[1]) * 60 + parseInt(partTime[2]);
   let totalTime = total.split(":");
-  let totalTimeSec = (totalTime[0] * 60 + totalTime[1]) * 60 + totalTime[2];
+  console.log(totalTime);
+  let totalTimeSec =
+    (totalTime[0] * 60 + totalTime[1]) * 60 + parseInt(totalTime[2]);
 
-  return [Math.round(totalTimeSec / partTimeSec)];
+  let upPart = partTimeSec,
+    downPart = totalTimeSec;
+
+  console.log(upPart, downPart);
+  for (let u = partTimeSec / 2; u >= 2; u--) {
+    if (upPart % u < 0.001 * upPart && downPart % u < 0.001 * downPart) {
+      upPart = Math.round(upPart / u);
+      downPart = Math.round(downPart / u);
+      u++;
+    }
+  }
+
+  return [upPart, downPart];
 }
-console.log(videoPart("02:20:00", "07:00:00"));
-console.log(videoPart("25:26:20", "25:26:20"));
-console.log(videoPart("00:02:20", "00:10:00"));
+console.log(videoPart("02:20:00", "07:00:00")); //1/3
+console.log(videoPart("25:26:20", "25:26:20")); //1/1
+console.log(videoPart("00:02:20", "00:10:00")); //7/30
